377b37bcbe480e345d01acac6cd13f7a8378f094
jaeyun95 <wodbs9522@gmail.com>
2019-12-01 00:05:51 +0900

    add programmers level2

---
+++ level2/level2_ex24.py
@@ -0,0 +1,20 @@
+#(24) 숫자의 표현
+
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        sum = 0
+        for j in range(i,n+1):
+            sum += j 
+            if sum == n: 
+                answer += 1
+                break
+            elif sum > n: break
+    return answer
+	
+	
+#다른 코드
+'''
+def solution(n):
+    return len([i  for i in range(1,n+1,2) if n % i is 0])
+'''
