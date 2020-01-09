2dc340e7c19ff28ec25239c897a2f3ad1ef56dcc
jaeyun95 <wodbs9522@gmail.com>
2020-01-10 00:23:23 +0900

    add programmers level3

---
+++ level3/level3_ex08.py
@@ -0,0 +1,8 @@
+#(8) 멀리 뛰기
+
+def solution(n):
+    if n < 3: return n
+    value1, value2 = 1, 2
+    for i in range(3,n+2):
+        value1, value2 = value2, value1 + value2
+    return value1%1234567
