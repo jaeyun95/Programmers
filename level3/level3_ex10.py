a8da6e5568ca0666f98ac23c30c1cc575d77f3b4
jaeyun95 <wodbs9522@gmail.com>
2020-01-12 03:00:04 +0900

    add programmers level3

---
+++ level3/level3_ex10.py
@@ -0,0 +1,34 @@
+#(10) 이중우선순위큐
+
+import heapq
+def solution(operations):
+    heap = []
+    for operation in operations:
+        command, num = operation.split(' ')
+        if command == 'I':
+            heapq.heappush(heap,int(num))
+        elif command == 'D' and len(heap) > 0:
+            if num == '1':
+                heap.pop(heap.index(heapq.nlargest(1,heap)[0]))
+            else:
+                heapq.heappop(heap)
+    if len(heap) == 0: return [0,0]
+    return [heapq.nlargest(1,heap)[0],heapq.nsmallest(1,heap)[0]]
+
+#다른 코드
+'''
+def solution(operations):
+    heap = []
+    for operation in operations:
+        command, num = operation.split(' ')
+        if command == 'I':
+            heap.append(int(num))
+        elif command == 'D' and len(heap) > 0:
+            if num == '1':
+                heap.pop()
+            else:
+                heap.pop(0)
+        heap.sort()
+    if len(heap) == 0: return [0,0]
+    return [max(heap),min(heap)]
+'''
