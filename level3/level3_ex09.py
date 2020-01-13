336dfd2c09db8232a0eb35e0227a67c0d6576d15
jaeyun95 <wodbs9522@gmail.com>
2020-01-14 03:03:55 +0900

    add programmers level3

--- level3/level3_ex09.py
+++ level3/level3_ex09.py
@@ -1,15 +1,34 @@
-#(9) 야근 지수
+#(9) 이중우선순위큐
 
 import heapq
-def solution(n, works):
-    if sum(works) < n: return 0
+def solution(operations):
     heap = []
-    for value in works:
-        heapq.heappush(heap,(-value,value))
-    print(heap)
-    for i in range(n):
-        update = heapq.heappop(heap)
-        value = update[1] - 1
-        heapq.heappush(heap,(-value,value))
-    works = [num[1]**2 for num in heap]
-    return sum(works)
+    for operation in operations:
+        command, num = operation.split(' ')
+        if command == 'I':
+            heapq.heappush(heap,int(num))
+        elif command == 'D' and len(heap) > 0:
+            if num == '1':
+                heap.pop(heap.index(heapq.nlargest(1,heap)[0]))
+            else:
+                heapq.heappop(heap)
+    if len(heap) == 0: return [0,0]
+    return [heapq.nlargest(1,heap)[0],heapq.nsmallest(1,heap)[0]]
+
+#다른 코드
+'''
+def solution(operations):
+    heap = []
+    for operation in operations:
+        command, num = operation.split(' ')
+        if command == 'I':
+            heap.append(int(num))
+        elif command == 'D' and len(heap) > 0:
+            if num == '1':
+                heap.pop()
+            else:
+                heap.pop(0)
+        heap.sort()
+    if len(heap) == 0: return [0,0]
+    return [max(heap),min(heap)]
+'''
