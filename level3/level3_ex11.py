432d90ba6f345b8675d83d68409a4e96de263078
jaeyun95 <wodbs9522@gmail.com>
2020-01-18 16:15:57 +0900

    add programmers level3

--- level3/level3_ex11.py
+++ level3/level3_ex11.py
@@ -15,3 +15,21 @@ def solution(tickets):
             now.append(information[top][-1])
             information[top] = information[top][:-1]
     return answer[::-1]
+	
+#다른 코드
+'''
+def solution(tickets):
+    def helper(tickets, route):
+        if tickets == []:
+            return route
+        left = [i for i in range(len(tickets)) if tickets[i][0] == route[-1]]
+        if left == []:
+            return None
+        left.sort(key = lambda i: tickets[i][1])
+
+        for next in left:
+            rest = helper(tickets[:next]+tickets[next+1:], route+[tickets[next][1]])
+            if rest is not None:
+                return rest
+    return helper(tickets, ["ICN"])
+'''
