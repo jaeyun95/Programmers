475e3663289b341090252c7e4e9c681d95b54594
jaeyun95 <wodbs9522@gmail.com>
2020-01-20 20:31:35 +0900

    add programmers level3

---
+++ level3/level3_ex14.py
@@ -0,0 +1,14 @@
+#(14) 줄 서는 방법
+
+import math
+def solution(n, k):
+    answer = [num for num in range(1,n+1)]
+    answer_list = []
+    while n > 0 :
+        n -= 1
+        share, remainder = divmod(k,math.factorial(n))
+        if not remainder: share -= 1
+        answer_list.append(answer[share])
+        answer.remove(answer[share])
+        k = remainder
+    return answer_list
