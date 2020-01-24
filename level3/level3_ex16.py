e2f3efc1f15b7608e302d5a65de4662b9ba77012
jaeyun95 <wodbs9522@gmail.com>
2020-01-25 00:49:19 +0900

    add programmers level3

---
+++ level3/level3_ex16.py
@@ -0,0 +1,38 @@
+#(16) [1차]추석 트래픽
+
+def count(start, times):
+    start = start
+    end = start + 1000
+    count = 0
+    for time in times:
+        if not (time[1] < start or time[0] >= end): count += 1
+    return count
+
+def solution(lines):
+    answer = 0
+    times = []
+    for line in lines:
+        time, demand = line.split()[1:]
+        hh, mm, ss = time.split(':')
+        time = int(hh)*3600*1000 + int(mm)*60*1000 + int(ss[:2]+ss[3:])
+        demand = demand[:-1].split('.')
+        if len(demand) > 1:
+            demand = int(demand[0])*1000 + int(demand[1]+('0'*(3-len(demand[1]))))
+        else: demand = int(demand[0])*1000
+        times.append([time-demand+ 1, time])
+
+    for time in times:
+        now = max(count(time[0],times),count(time[1],times))
+        if answer < now: answer = now
+    return answer
+	
+#다른 풀이
+'''
+def solution(n):
+    answer = [0]
+    for i in range(1,n):
+        k = [1-j for j in answer]
+        answer = answer + [0] + k[::-1]
+
+    return answer
+'''
