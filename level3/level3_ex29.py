632379d1f8da8eea6b3f2fee05903f8b6f7c92ca
jaeyun95 <wodbs9522@gmail.com>
2020-03-22 21:37:31 +0900

    add programmers level3

---
+++ level3/level3_ex29.py
@@ -0,0 +1,20 @@
+#(29) N으로 표현
+
+def solution(N, number):
+    answer = -1
+    S = [set() for x in range(8)]
+    for i,x in enumerate(S, start=1):
+        x.add(int(str(N)*i))
+    for i in range(1, len(S)):
+        for j in range(i):
+            for op1 in S[j]:
+                for op2 in S[i-j-1]:
+                    S[i].add(op1 + op2)
+                    S[i].add(op1 - op2)
+                    S[i].add(op1 * op2)
+                    if op2 != 0:
+                        S[i].add(op1 // op2)
+        if number in S[i]:
+            answer = i + 1
+            break
+    return answer
